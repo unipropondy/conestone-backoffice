353c4f6f435436f45ddf59566f7f11e8c794d1f2
bavani18 <bavaniabavani18@gmail.com>
2026-07-09 12:58:27 +0530

    membermaster add

--- src/config/config.js
+++ src/config/config.js
@@ -4,4 +4,4 @@ export const API_BASE_URL = "https://conestone-backoffice-production.up.railway.
 
 
 
-export const API_BASE_URL = "http://localhost:3002";
+// export const API_BASE_URL = "http://localhost:3002";
