3b4a307cee7ffaf8c3f856676d30c7cf5b2f3924
bavani18 <bavaniabavani18@gmail.com>
2026-07-15 09:31:22 +0530

    promocode

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 
 
-// export const BASE_URL = "https://conestone-backoffice-production.up.railway.app";
+export const BASE_URL = "https://conestone-backoffice-production.up.railway.app";
 
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
 
 // const BASE_URL = "https://smabackoffice-production-fc6a.up.railway.app";
 
